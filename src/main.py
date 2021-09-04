0c06264c45f0c1d5cead12500d405a7e880a2d54
Shpm21 <galleguilloscristian2@gmail.com>
2021-09-04 09:25:40 -0400

    create Game class

--- src/main.py
+++ src/main.py
@@ -3,26 +3,42 @@ from bullet.Bullet import Bullet
 
 
 pygame.init()
-screen = pygame.display.set_mode((1080, 720))
-clock = pygame.time.Clock()
-balas = pygame.sprite.Group()
-background = pygame.Surface((1080, 720))
-salida = False
-while not salida:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            salida = True
-        pressed = pygame.key.get_pressed()
-        if pressed[pygame.K_SPACE]:
-            bala = Bullet((50, 50))
-            balas.add(bala)
 
-    screen.blit(background, (0, 0))
-    balas.update()
-    balas.draw(screen)
-    pygame.display.flip()
-    clock.tick(60)
 
-pygame.quit()
+class Game():
+    def __init__(self):
+        self.width = 1080
+        self.height = 720
+        self.screen = pygame.display.set_mode((self.width, self.height))
+        self.clock = pygame.time.Clock()
+        self.bullets = pygame.sprite.Group()
+        self.background = pygame.Surface((self.width, self.height))
+        self.salida = False
+        self.run()
+
+    def run(self):
+        while not self.salida:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.salida = True
+                pressed = pygame.key.get_pressed()
+                if pressed[pygame.K_SPACE]:
+                    bullet = Bullet((50, 50))
+                    self.bullets.add(bullet)
+
+            self.screen.blit(self.background, (0, 0))
+            self.bullets.update()
+            self.bullets.draw(self.screen)
+            pygame.display.flip()
+            self.clock.tick(60)
+
+        pygame.quit()
+
+
+def execute():
+    game = Game()
+
+
+execute()
 print("FIN")
 exit()
